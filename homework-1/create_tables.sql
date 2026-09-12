74c3754c9b7ba2cb751dfba8c5d3b95888a0f816
WebSprint <dovudusmonov24@gmail.com>
2026-09-12 18:34:38 +0500

    Update create_tables.sql

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,30 @@
 -- SQL-команды для создания таблиц
+ 
+DROP TABLE IF EXISTS orders;
+DROP TABLE IF EXISTS customers;
+DROP TABLE IF EXISTS employees;
+ 
+CREATE TABLE customers (
+    customer_id VARCHAR(5) PRIMARY KEY,
+    company_name VARCHAR(100) NOT NULL,
+    contact_name VARCHAR(100)
+);
+ 
+CREATE TABLE employees (
+    employee_id INTEGER PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    title VARCHAR(100),
+    birth_date DATE,
+    notes TEXT
+);
+ 
+CREATE TABLE orders (
+    order_id INTEGER PRIMARY KEY,
+    customer_id VARCHAR(5) REFERENCES customers(customer_id),
+    employee_id INTEGER REFERENCES employees(employee_id),
+    order_date DATE,
+    ship_city VARCHAR(100)
+);
+ 
+-- SQL-команды для создания таблиц
